ddfc679051f60d00c03150e9c8d2e60dc9c64758
javagurulv <javagurulv@gmail.com>
2023-09-28 18:22:51 +0300

    LESSON 1

--- src/main/java/lv/javaguru/java1/teacher/lesson_1_introduction/lessoncode/HelloWorld.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_1_introduction/lessoncode/HelloWorld.java
@@ -3,7 +3,6 @@ package lv.javaguru.java1.teacher.lesson_1_introduction.lessoncode;
 class HelloWorld {
 
 
-
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
