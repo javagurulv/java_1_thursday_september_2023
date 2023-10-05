26078d03626c6358cf5c7b95c8dcd40c1aa42e90
milans <milansmicko14@gmail.com>
2023-10-05 20:46:21 +0300

    first to my programs

---
+++ src/main/java/lv/javaguru/java1/student_milans_micko/lesson_2_variables/lessoncode/PrintFirstAndLastName.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_milans_micko.lesson_2_variables.lessoncode;
+
+public class PrintFirstAndLastName {
+
+    public static void main(String[] args) {
+        System.out.println("Milan");
+        System.out.println("Micko");
+        System.out.println("ppp");
+    }
+
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_milans_micko/lesson_2_variables/lessoncode/PrintMyName.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_milans_micko.lesson_2_variables.lessoncode;
+
+public class PrintMyName {
+    public static void main(String[] args) {
+        System.out.println("Milan");
+    }
+
+}
