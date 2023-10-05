f41be13dec8952d18a6cf99449cf3a9e171761ca
Wanxezar <zenadik@inbox.lv>
2023-10-05 21:47:10 +0300

    lessoncode 2, exercises

---
+++ src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_2_variables/lessoncode/Calc.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_daniels_demishins.lesson_2_variables.lessoncode;
+
+public class Calc {
+    public static void main(String[] args) {
+        int numA = 10;
+        int numB = 5;
+
+        int sum = numA + numB;
+        int min = numA - numB;
+        int mul = numA * numB;
+        int div = numA / numB;
+
+        System.out.println("Result + = " + sum);
+        System.out.println("Result - = " + min);
+        System.out.println("Result * = " + mul);
+        System.out.println("Result / = " + div);
+    }
+}
