1877b5312c3a4c8d3960ad8b022b3e8abb7c3422
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-05 20:46:03 +0300

    first 3 programs

---
+++ src/main/java/lv/javaguru/java1/kirils_petrovs/lesson_2_variables/lessoncode/PrintMyNameAndSurname.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.kirils_petrovs.lesson_2_variables.lessoncode;
+
+public class PrintMyNameAndSurname {
+
+
+    public static void main(String[] args) {
+        System.out.println("Kirils");
+        System.out.println("Petrovs");
+    }
+
+}
