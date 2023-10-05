fda49b499f4a41fd1331fbb1e793fccdaaf5e96b
disert <disert@inbox.lv>
2023-10-05 23:05:33 +0600

    first two programs

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_2_variables/lessoncode/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_2_variables.lessoncode;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+        //Zadatj chisla
+        int number1 = 738732;  // -2 (31) - +2(31)
+        int number2 = 777999;
+
+        // Slozitj chisla
+        int result = number1 + number2;
+
+        // vivesti na konsolj
+        System.out.println("Result = " + result);
+    }
+
+
+
+
+    }
