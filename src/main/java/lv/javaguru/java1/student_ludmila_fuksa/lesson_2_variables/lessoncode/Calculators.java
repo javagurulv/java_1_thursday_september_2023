83184facdc5e4fe09046b5a8d0a4c35d5b3a3bf1
LudmilaJava <lbf@inbox.lv>
2023-10-05 21:46:25 +0300

    lesson work

---
+++ src/main/java/lv/javaguru/java1/student_ludmila_fuksa/lesson_2_variables/lessoncode/Calculators.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_ludmila_fuksa.lesson_2_variables.lessoncode;
+
+public class Calculators {
+    public static void main(String[] args) {
+        int number1 = 1000;
+        int number2 =  200;
+
+        int sumResult = number1 + number2;
+        int minusResult = number1 - number2;
+        int multiplyResult = number1 * number2;
+        int divisionResult = number1 / number2;
+
+        System.out.println("Result =" + sumResult);
+        System.out.println("Result =" + minusResult);
+        System.out.println("Result =" + multiplyResult);
+        System.out.println("Result =" + divisionResult);
+    }
+
+}
