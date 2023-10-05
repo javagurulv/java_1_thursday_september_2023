2b5406c6033046419eeeef4999da67103b8b524e
disert <disert@inbox.lv>
2023-10-05 20:50:30 +0600

    first two programs

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_2_variables/lessoncode/PrintFirstAndLastName.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_2_variables.lessoncode;
+
+public class PrintFirstAndLastName {
+    public static void main(String[] args) {
+        System.out.println("Sergej");
+        System.out.println("Roslakov");
+    }
+
+}
