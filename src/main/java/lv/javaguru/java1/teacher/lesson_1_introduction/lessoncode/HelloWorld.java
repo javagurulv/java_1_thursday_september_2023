da62db22ff42a35b9a9ae6dbcdc07ac715cca32a
Valerijs <valerka1176@gmail.com>
2023-10-05 21:08:16 +0300

    first work

--- src/main/java/lv/javaguru/java1/teacher/lesson_1_introduction/lessoncode/HelloWorld.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_1_introduction/lessoncode/HelloWorld.java
@@ -4,7 +4,8 @@ class HelloWorld {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
+        System.out.println("Valera");
+        System.out.println("Kovs");
 
+    }
 }
---
+++ src/main/java/lv/javaguru/java1/valera_kovs/lesson_1_introduction/lessoncode/PrintMyName.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.valera_kovs.lesson_1_introduction.lessoncode;
+
+public class PrintMyName {public static void main(String[] args) {
+    System.out.println("Viktor");
+}
+
+
+
+}
