363d74d4642193cfc4df3186001d38c5b5849288
ekvilumane <elizabethvilyumane8@gmail.com>
2023-10-05 20:46:50 +0300

    First two programs

---
+++ src/main/java/lv/javaguru/java1/student_elizabeth_vilumane/lesson_2_variables/lessoncode/PrintMyName.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_elizabeth_vilumane.lesson_2_variables.lessoncode;
+
+public class PrintMyName {
+
+    public static void main(String[] args) {
+        System.out.println("Elizabeth");
+    }
+
+}
