32b739cfe71f90e86e86d56c5af649cfab8b87a2
viktors.aleksejevs <viktors6@inbox.lv>
2023-10-05 21:59:24 +0300

    CalculatorFigures

---
+++ src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/lessoncode/CalculatorFigures.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_viktors_aleksejevs.lesson_2_variables.lessoncode;
+
+public class CalculatorFigures {
+    public static void main(String[] args){
+
+        int number1 = 1000;
+        int number2 = 200;
+
+        int sumResult = number1 + number2;
+        int minusResult = number1 - number2;
+        int multiplyResult = number1 * number2;
+        int divisionResult = number1 / number2;
+
+        System.out.println("Result + = " + sumResult);
+        System.out.println("Result - = " + minusResult);
+        System.out.println("Result * = " + multiplyResult);
+        System.out.println("Result / = " + divisionResult);
+    }
+
+
+}
