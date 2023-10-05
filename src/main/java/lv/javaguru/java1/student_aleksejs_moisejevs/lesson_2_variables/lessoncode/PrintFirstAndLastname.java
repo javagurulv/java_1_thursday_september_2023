2e6f37fadc84081799bf80687637b29efa8583f4
amois <a.moisejevs@live.com>
2023-10-05 20:46:40 +0300

    Print First Name Print First Name and Last Name

---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_2_variables/lessoncode/PrintFirstAndLastname.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_2_variables.lessoncode;
+
+public class PrintFirstAndLastname {
+
+    public static void main(String[] args) {
+        System.out.println("Aleksejs");
+        System.out.println("Moisejevs");
+    }
+
+}
