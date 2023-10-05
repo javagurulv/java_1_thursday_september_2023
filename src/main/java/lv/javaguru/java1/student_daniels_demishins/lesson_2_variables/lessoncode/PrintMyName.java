89035a7913cf8143713532e67ac64e2c8dc99142
Wanxezar <zenadik@inbox.lv>
2023-10-05 20:45:52 +0300

    lessoncode 2, 2 exercises

---
+++ src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_2_variables/lessoncode/PrintMyName.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_daniels_demishins.lesson_2_variables.lessoncode;
+
+public class PrintMyName {
+    public static void main(String[] args) {
+        System.out.println("Daniel");
+    }
+}
