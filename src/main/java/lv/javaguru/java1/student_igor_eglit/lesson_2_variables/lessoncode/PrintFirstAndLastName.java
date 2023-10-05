cfe7699b874170826e0d8b9be480a2b629c09c7d
Игорь Эглит <igeglits@gmail.com>
2023-10-05 20:45:58 +0300

    System.out.println(""); Java programs

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/lessoncode/PrintFirstAndLastName.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_2_variables.lessoncode;
+
+public class PrintFirstAndLastName {
+
+    public static void main(String[] args) {
+        System.out.println("Igor");
+        System.out.println("Eglit");
+    }
+
+
+
+
+}
