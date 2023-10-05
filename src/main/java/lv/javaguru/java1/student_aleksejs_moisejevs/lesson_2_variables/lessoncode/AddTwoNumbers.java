78193ad4236babfede559a3f07a79d75cd6faeb1
amois <a.moisejevs@live.com>
2023-10-05 21:23:50 +0300

    AddTwoNumbers

---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_2_variables/lessoncode/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_2_variables.lessoncode;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 200;
+        int result  = number1 + number2;
+        System.out.println(" result = " + result);
+    }
+
+}
