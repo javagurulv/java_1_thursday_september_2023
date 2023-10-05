6bed90e284b7b82fae8f5bfdef32d0474aa4d13f
milans <milansmicko14@gmail.com>
2023-10-05 21:59:01 +0300

    first my calculator

---
+++ src/main/java/lv/javaguru/java1/student_milans_micko/lesson_2_variables/lessoncode/Calculator.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java1.student_milans_micko.lesson_2_variables.lessoncode;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 5;
+
+
+        int sumresult = number1 + number2;
+        int subtractionResult = number1 - number2;
+        int multiplyResult = number1 * number2;
+        int divisionResult = number1 / number2;
+
+
+        System.out.println("Result = " + sumresult);
+        System.out.println("Result = " + subtractionResult);
+        System.out.println("Result = " + multiplyResult);
+        System.out.println("Result = " + divisionResult);
+
+}
+
+
+
+}
