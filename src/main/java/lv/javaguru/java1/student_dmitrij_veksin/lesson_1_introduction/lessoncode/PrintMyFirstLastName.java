e85cf69e9c666331147544ab94575fd66aea1af3
PlagueDy <vek6in@gmail.com>
2023-10-05 18:14:20 +0300

    Lesson II (PrintFirstLastName)

---
+++ src/main/java/lv/javaguru/java1/student_dmitrij_veksin/lesson_1_introduction/lessoncode/PrintMyFirstLastName.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_dmitrij_veksin.lesson_1_introduction.lessoncode;
+
+public class PrintMyFirstLastName{
+    public static void main(String[]args){
+        System.out.println("Dmitry");
+        System.out.println("Veksin");
+    }
+}
+
