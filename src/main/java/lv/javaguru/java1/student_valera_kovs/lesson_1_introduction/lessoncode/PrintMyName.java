4e8923c562d1f675d2dee4f589e7881ae07727f7
javagurulv <javagurulv@gmail.com>
2023-10-06 09:27:52 +0300

    fix errors

--- src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/PrintMyName.java
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/PrintMyName.java
@@ -1,4 +1,4 @@
-package lv.javaguru.java1.valera_kovs.lesson_1_introduction.lessoncode;
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.lessoncode;
 
 public class PrintMyName {public static void main(String[] args) {
     System.out.println("Viktor");
