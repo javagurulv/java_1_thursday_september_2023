f2d5676bf22ed42b6652359027437c24e7e3e5a8
Игорь Эглит <igeglits@gmail.com>
2023-10-06 13:31:03 +0300

    homework level_2_intern

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_1_intern/InputPrintName.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_2_variables.homework.level_1_intern;
+import java.util.Scanner;
+public class InputPrintName {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        //nextLine(): считывает всю введенную строку
+        System.out.println("Hello " + name + " !");
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_1_intern/LoginService.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_2_variables.homework.level_1_intern;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
