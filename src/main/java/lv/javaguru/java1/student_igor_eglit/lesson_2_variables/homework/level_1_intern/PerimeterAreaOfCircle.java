c59b86c3be3fc0fb049a45513825c51f6798fcfb
Игорь Эглит <igeglits@gmail.com>
2023-10-07 16:21:44 +0300

    fixed/working периметр и площадь окружности

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_1_intern/PerimeterAreaOfCircle.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_1_intern/PerimeterAreaOfCircle.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.lang.Math;
 import static java.lang.Math.pow;
 
+
 //импорт объектов класса Math
 //Библиотека классов java.lang.Math содержит константы и методы,
 // нужные для выполнения математических вычислений.
@@ -10,9 +11,13 @@ public class PerimeterAreaOfCircle {
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Radius = ");
-        //Программа работает пока только с целыми числами
-        float radius = in.nextFloat();
+        System.out.print("Radius = ");
+
+        //System.out.print выводит поле ввода в той же строке, System.out.println в следующей
+        //почему на моем компьютере float number работают в Idea с запятой, а не с точкой?!!!!!
+        double radius = in.nextDouble();
+        System.out.println();//вывод расчетов через пустую строку в консоле
+
         double perimeter = 2 * Math.PI * radius;
         double area = Math.PI * pow(radius,2);
         System.out.println("Perimeter is = " + perimeter);
