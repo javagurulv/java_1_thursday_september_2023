fa2e1a8ccece13ba63413aa2cc32f5db242c8faf
Valerijs <valerka1176@gmail.com>
2023-10-07 20:40:17 +0300

    homework

--- src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/calkulator01.java
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/calkulator01.java
@@ -3,20 +3,21 @@ package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.lessoncode;
 import java.util.Scanner;
 
     public class calkulator01 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            //S=p(p-a)(p-b)(p-c)
-            System.out.println("triangle1");
-            double triangle1 = scanner.nextDouble();
 
-            System.out.println("triangle2");
-            double triangle2 = scanner.nextDouble();
 
-            System.out.println("triangle3");
-            double triangle3 = scanner.nextDouble();
+            public static void main(String[] args) {
+                int number1 = 738732;
+                int number2 = 777999;
 
-            double perimetr = 0.5 * (triangle1+triangle2+triangle3);
-            double area = perimetr * (perimetr - triangle1)*(perimetr-triangle2)*(perimetr-triangle3);
-            System.out.println("area = " + area);
-        }
+                int sumResult = number1 + number2;
+                int minusResult = number1 - number2;
+                int multiplyResult = number1 * number2;
+                int divisionResult = number1 / number2;
+
+                System.out.println("Result + = " + sumResult);
+                System.out.println("Result - = " + minusResult);
+                System.out.println("Result * = " + multiplyResult);
+                System.out.println("Result / = " + divisionResult);
+
+            }
 }
