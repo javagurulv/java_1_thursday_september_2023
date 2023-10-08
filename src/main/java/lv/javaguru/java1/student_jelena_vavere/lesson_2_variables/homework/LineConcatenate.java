15880d062a066c57b2b9342cb1a6adc9b6e2661d
jelenavavere <jelenavavere@inbox.lv>
2023-10-08 22:45:44 +0300

    LineConcatenate

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_2_variables/homework/LineConcatenate.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_2_variables.homework;
+
+public class LineConcatenate {
+        public static void main(String[] args) {
+
+                String firstName = "Vasja";
+                String lastName = "Pupkin";
+                String result = firstName + lastName;
+                System.out.println("Print Full Name - " + result );
+
+
+        }
+}
