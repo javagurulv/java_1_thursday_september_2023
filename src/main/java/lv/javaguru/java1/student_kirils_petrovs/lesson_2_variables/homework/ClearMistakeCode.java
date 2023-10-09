0c457c99030d0c0bb469638390a337c6f1b00ace
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 14:37:02 +0300

    Home Work Clear Mistake Code Level1

--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeCode.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeCode.java
@@ -1,18 +1,8 @@
-package lv.javaguru.java1.kirils_petrovs.lesson_2_variables.homework;
+package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 
-//Дан код, нужно исправить синтаксическую ошибку,
-//которая приводит к ошибке компиляции.
-//Функционал класса писать не надо.
-//Для сдачи работы создайте исправленный класс.
-//
-//class Book {
-//
-//
-//Цель этого задания: обратить ваше внимание на синтаксис
-//объявления класса в языке программирования Java.
 public class ClearMistakeCode {
 
-    public class Book {
+    public static class Book {
 
     }
 
