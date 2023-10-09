fdcb305e56fec082c70344f807495027c82b207f
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 17:26:36 +0300

    Home_Work_LV1

--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task2.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task2.java
@@ -2,14 +2,13 @@ package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 
 public class Home_Work_LV_1_Task2 {
 
-    public class Phone {
+    public static class Phone {
 
         public static void main(String[] args) {
 
         }
     }
 }
-
 
 
 
--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task4.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task4.java
@@ -2,14 +2,13 @@ package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 
 public class Home_Work_LV_1_Task4 {
 
-    public class Turtle {
+    public static class Turtle {
 
         public static void main(String[] args) {
 
         }
     }
 }
-
 
 
 
