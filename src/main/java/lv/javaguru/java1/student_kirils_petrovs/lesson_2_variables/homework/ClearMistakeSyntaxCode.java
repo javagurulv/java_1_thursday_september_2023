69cbf785fd4a169baf8b49bbae1791fc55eb47b2
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 13:07:50 +0300

    Home Work Clear Mistake Code Level1

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeSyntaxCode.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.kirils_petrovs.lesson_2_variables.homework;
+
+public class ClearMistakeSyntaxCode {
+
+    public class BankAccount {
+        public static void main(String[] args) {
+
+        }
+
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeSyntaxCodeTesk_4.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.kirils_petrovs.lesson_2_variables.homework;
+
+public class ClearMistakeSyntaxCodeTesk {
+
+    public class Turtle {
+
+        public static void main(String[] args) {
+
+    }
+
+}
+
