8a3d4b51bf0ae846d53e62242c362ad1acd1b2a6
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 15:25:52 +0300

    Home Work Clear Mistake Code Level1

--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeCode.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/ClearMistakeCode.java
@@ -1,12 +1,7 @@
 package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 
-public class ClearMistakeCode {
+public class ClearMistakeCode_Task1_LV1 {
 
     public static class Book {
 
     }
-
-
-}
-
-
