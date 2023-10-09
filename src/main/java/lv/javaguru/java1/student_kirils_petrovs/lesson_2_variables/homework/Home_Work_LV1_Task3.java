83810321e0b3e7299c495dbb5404d4c41dd300a7
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 16:52:08 +0300

    Home_Work_LV1

--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV1_Task3.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV1_Task3.java
@@ -3,10 +3,10 @@ package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 public class Home_Work_LV1_Task3 {
 
 
-   public class BankAccount {
+    public static class BankAccount {
 
         public static void main(String[] args) {
 
         }
-   }
+    }
 }
