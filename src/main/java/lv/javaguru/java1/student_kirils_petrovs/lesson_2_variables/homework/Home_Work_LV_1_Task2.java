78d51c0b84929396bcc972ea93fcec343ecf653f
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-09 17:30:07 +0300

    Home_Work_LV1

--- src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task2.java
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV_1_Task2.java
@@ -2,7 +2,7 @@ package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
 
 public class Home_Work_LV_1_Task2 {
 
-    public static class Phone {
+    public class Phone {
 
         public static void main(String[] args) {
 
