7fd0abf028f97896a8148a14d4b181621cc47290
milans <milansmicko14@gmail.com>
2023-10-09 19:13:10 +0300

    homework 09.10

---
+++ src/main/java/lv/javaguru/java1/student_milans_micko/lesson_2_variables/homework/lvl_1/class_phone.java
@@ -0,0 +1,4 @@
+package lv.javaguru.java1.student_milans_micko.lesson_2_variables.homework.lvl_1;
+
+public class class_phone {
+}
---
+++ src/main/java/lv/javaguru/java1/student_milans_micko/lesson_2_variables/homework/lvl_4/mouse_alternative.java
@@ -0,0 +1,4 @@
+package lv.javaguru.java1.student_milans_micko.lesson_2_variables.homework.lvl_4;
+
+public class mouse_alternative {
+}
