40725e0826bb63ea2a05ce5389c4182fc3da711a
javagurulv <javagurulv@gmail.com>
2023-10-10 22:45:14 +0300

    fix errors

--- src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/homework/Hello.java
+++ src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/homework/Hello.java
@@ -1,7 +1,9 @@
 package lv.javaguru.java1.student_viktors_aleksejevs.lesson_2_variables.homework;
 
 import java.util.Scanner;
+
 public class Hello {
+
     public static void main(String[] args) {
 
         System.out.println("What is your name?");
@@ -10,7 +12,6 @@ public class Hello {
         System.out.println("Hello " + Viktors);
 
     }
+
+
 }
-
-
-
--- src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/homework/Multi8.java
+++ src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/homework/Multi8.java
@@ -13,27 +13,27 @@ public class Multi8 {
         int number9 = 9;
         int number10 = 10;
 
-        int multiplyResult = number8 * number1;
-        int multiplyResult = number8 * number2;
-        int multiplyResult = number8 * number3;
-        int multiplyResult = number8 * number4;
-        int multiplyResult = number8 * number5;
-        int multiplyResult = number8 * number6;
-        int multiplyResult = number8 * number7;
-        int multiplyResult = number8 * number8;
-        int multiplyResult = number8 * number9;
-        int multiplyResult = number8 * number10;
+        int multiplyResult1 = number8 * number1;
+        int multiplyResult2 = number8 * number2;
+        int multiplyResult3 = number8 * number3;
+        int multiplyResult4 = number8 * number4;
+        int multiplyResult5 = number8 * number5;
+        int multiplyResult6 = number8 * number6;
+        int multiplyResult7 = number8 * number7;
+        int multiplyResult8 = number8 * number8;
+        int multiplyResult9 = number8 * number9;
+        int multiplyResult10 = number8 * number10;
 
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
-        System.out.println("Result * = " + multiplyResult);
+        System.out.println("Result * = " + multiplyResult1);
+        System.out.println("Result * = " + multiplyResult2);
+        System.out.println("Result * = " + multiplyResult3);
+        System.out.println("Result * = " + multiplyResult4);
+        System.out.println("Result * = " + multiplyResult5);
+        System.out.println("Result * = " + multiplyResult6);
+        System.out.println("Result * = " + multiplyResult7);
+        System.out.println("Result * = " + multiplyResult8);
+        System.out.println("Result * = " + multiplyResult9);
+        System.out.println("Result * = " + multiplyResult10);
     }
 }
 
