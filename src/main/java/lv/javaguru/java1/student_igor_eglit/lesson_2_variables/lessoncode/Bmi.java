0631360b2bc2177c4908af56e69f345b93508e3d
Игорь Эглит <igeglits@gmail.com>
2023-10-10 15:10:44 +0300

    BMI task 12

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/lessoncode/Bmi.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_2_variables.lessoncode;
+import java.util.Scanner;
+public class Bmi {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Your weight in kilograms:  ");
+        int weight = in.nextInt();
+        System.out.print("Your height in meters:  ");
+        double height = in.nextDouble();
+
+        double bmi = weight/Math.pow(height,2);
+        System.out.println("Your BMI is " + bmi);
+
+
+       /* if(0>bmi<=18.5) {
+            System.out.println("недостаточный вес");}
+        else if (18.5<bmi<=25){
+        System.out.println("нормальный вес");
+        }*/
+    }
+}
