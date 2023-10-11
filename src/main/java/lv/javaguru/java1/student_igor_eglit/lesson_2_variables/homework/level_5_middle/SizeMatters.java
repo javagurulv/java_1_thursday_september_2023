5668d4df038a9570a4fdc494729669646e497276
Игорь Эглит <igeglits@gmail.com>
2023-10-11 11:53:35 +0300

    second commit

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_5_middle/SizeMatters.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_2_variables/homework/level_5_middle/SizeMatters.java
@@ -5,5 +5,6 @@ public class SizeMatters {
     public static void main(String[] args) {
         System.out.println("Sorry, I have an errors.");
         System.out.println("Please fix me!");
+
     }
 }
