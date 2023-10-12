8daabf92bf5a2e5282aedfa5d9dcf4b220f62df6
diska4er <diska4er@gmail.com>
2023-10-12 15:34:21 +0300

    level 3

---
+++ src/main/java/lv/javaguru/java1/student_artjoms_potasevskis/lesson_2_variables/homework_level3/Multiplication.java
@@ -0,0 +1,37 @@
+package lv.javaguru.java1.student_artjoms_potasevskis.lesson_2_variables.homework_level3;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+        System.out.println("Input number");
+        int number1 = 8;
+
+
+        int result1 = number1 * 1;
+        int result2 = number1 * 2;
+        int result3 = number1 * 3;
+        int result4 = number1 * 4;
+        int result5 = number1 * 5;
+        int result6 = number1 * 6;
+        int result7 = number1 * 7;
+        int result8 = number1 * 8;
+        int result9 = number1 * 9;
+        int result10 = number1 * 10;
+
+        System.out.println(number1 + " x 1 = " + result1);
+        System.out.println(number1 + " x 2 = " + result2);
+        System.out.println(number1 + " x 3 = " + result3);
+        System.out.println(number1 + " x 4 = " + result4);
+        System.out.println(number1 + " x 5 = " + result5);
+        System.out.println(number1 + " x 6 = " + result6);
+        System.out.println(number1 + " x 7 = " + result7);
+        System.out.println(number1 + " x 8 = " + result8);
+        System.out.println(number1 + " x 9 = " + result9);
+        System.out.println(number1 + " x 10 = " + result10);
+
+
+    }
+
+    }
