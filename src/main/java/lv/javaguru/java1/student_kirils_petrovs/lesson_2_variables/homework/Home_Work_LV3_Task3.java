388034c9415875d4460d4f363c166a94ec5e5fba
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-12 19:09:49 +0300

    Home_Work_LV3_Task_3

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_2_variables/homework/Home_Work_LV3_Task3.java
@@ -0,0 +1,59 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_2_variables.homework;
+
+
+
+public class Home_Work_LV3_Task3 {
+
+    public static void main(String[] args){
+        ;
+
+        int a= 3;
+        int b= 5;
+        int c= 4;
+
+
+
+        int e= a+b+c;
+
+
+
+
+
+        System.out.println("a="+a );
+        System.out.println("b="+b );
+        System.out.println("c="+c );
+        System.out.println("a+b+c=e");
+
+
+        System.out.println("e="+e);
+        System.out.println("e/e=f");
+        System.out.println("f="+e/e);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
