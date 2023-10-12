8d4996ac79af2528c409859788185cf5bba97625
javagurulv <javagurulv@gmail.com>
2023-10-12 21:08:42 +0300

    code

--- src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/PrintMyName.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/PrintMyName.java
@@ -2,8 +2,6 @@ package lv.javaguru.java1.teacher.lesson_2_variables.lessoncode;
 
 public class PrintMyName {
 
-    efsgfdbf
-
     public static void main(String[] args) {
         System.out.println("Viktor");
     }
