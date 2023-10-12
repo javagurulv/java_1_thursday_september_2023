6aa484e031a56e8741b903c96828c5c82ec16317
javagurulv <javagurulv@gmail.com>
2023-10-12 20:30:42 +0300

    ERROR!

--- src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/PrintMyName.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/PrintMyName.java
@@ -2,6 +2,8 @@ package lv.javaguru.java1.teacher.lesson_2_variables.lessoncode;
 
 public class PrintMyName {
 
+    efsgfdbf
+
     public static void main(String[] args) {
         System.out.println("Viktor");
     }
