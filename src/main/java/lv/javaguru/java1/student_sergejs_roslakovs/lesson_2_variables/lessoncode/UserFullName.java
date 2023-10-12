74676a50a844f8b2a8fdab7e0ae3f6d9ec962ef7
disert <disert@inbox.lv>
2023-10-12 21:24:34 +0600

    homework

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_2_variables/lessoncode/UserFullName.java
@@ -0,0 +1,45 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_2_variables.lessoncode;
+
+import java.util.Scanner;
+
+public class UserFullName {
+
+    // Runtime
+    // static code
+
+    // JDK - Java Development Kit 21
+
+    // java compiler  (.java) -> bytecode
+
+    // JVM - Java Virtual Machine
+
+    // bytecode
+
+    // lv.javaguru.java1.teacher
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter first name: ");
+        String lastName = scanner.nextLine();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
