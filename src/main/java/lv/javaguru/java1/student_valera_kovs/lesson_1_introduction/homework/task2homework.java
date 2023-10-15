7f9f3e25f13947146714583f88188f303bc4c31b
Valerijs <valerka1176@gmail.com>
2023-10-15 18:12:41 +0300

    homework.task.2

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/homework/task2homework.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.homework;
+
+class LoginService {
+
+        public static void main(String[] args) {
+            System.out.println("You can login!");
+
+        }
+    }
