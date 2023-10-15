05b1122c5c98fe4884b59b2c58619686d2f3368f
Valerijs <valerka1176@gmail.com>
2023-10-15 19:41:11 +0300

    homework.task.3.Lesson.3

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/homework/task3HardWorkLesson3.java
@@ -0,0 +1,37 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.homework;
+
+import java.util.Scanner;
+
+public class task3HardWorkLesson3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int number = in.nextInt();
+
+
+        int number1 = number * 1;
+        int number2 = number * 2;
+        int number3 = number * 3;
+        int number4 = number * 4;
+        int number5 = number * 5;
+        int number6 = number * 6;
+        int number7 = number * 7;
+        int number8 = number * 8;
+        int number9 = number * 9;
+        int number10 = number * 10;
+
+
+        System.out.println(number + " * 1 =" + number1);
+        System.out.println(number + " * 2 =" + number2);
+        System.out.println(number + " * 3 =" + number3);
+        System.out.println(number + " * 4 =" + number4);
+        System.out.println(number + " * 5 =" + number5);
+        System.out.println(number + " * 6 =" + number6);
+        System.out.println(number + " * 7 =" + number7);
+        System.out.println(number + " * 8 =" + number8);
+        System.out.println(number + " * 9 =" + number9);
+        System.out.println(number + " * 10 =" + number10);
+
+    }
+}
