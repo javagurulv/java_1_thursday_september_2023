46682279b8265ec5718a9384923d0f53d5eefd7a
Valerijs <valerka1176@gmail.com>
2023-10-15 19:53:33 +0300

    homework.task.4

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/homework/Task4HardHomework.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.homework;
+
+public class Task4HardHomework {
+
+    public static void main(String[] args) {
+        System.out.print("text");
+    }
+}
