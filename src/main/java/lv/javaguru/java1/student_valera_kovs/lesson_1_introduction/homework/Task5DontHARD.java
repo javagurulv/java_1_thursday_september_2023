9683833784214b67d16e904b6e44e54d8d5e99fb
Valerijs <valerka1176@gmail.com>
2023-10-15 20:00:32 +0300

    homework.task.5

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/homework/Task5DontHARD.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.homework;
+
+public class Task5DontHARD {
+    public static void main(String[] args) {
+        System.out.print("text");
+    }
+}
