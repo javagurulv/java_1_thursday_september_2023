b07b7700b6135e675872d5cc862ba7f24c85cd66
Valerijs <valerka1176@gmail.com>
2023-10-15 18:58:27 +0300

    homework.task.3.Lesson.1

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/homework/task3HardWork.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.homework;
+
+import java.util.Scanner;
+public class task3HardWork {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("number 1 = 8");
+        System.out.println("number 2 = 16");
+        System.out.println("number 3 = 24");
+        System.out.println("number 4 = 32");
+        System.out.println("number 5 = 40");
+        System.out.println("number 6 = 48");
+        System.out.println("number 7 = 56");
+        System.out.println("number 8 = 64");
+        System.out.println("number 9 = 72");
+        System.out.println("number 10 = 80");
+    }
+}
