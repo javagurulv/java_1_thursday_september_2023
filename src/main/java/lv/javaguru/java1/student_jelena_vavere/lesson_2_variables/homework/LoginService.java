d35758d72cbd0eb679281dec7d87ab74ef3635a4
jelenavavere <jelenavavere@inbox.lv>
2023-10-16 23:09:57 +0300

    LoginService

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_2_variables/homework/LoginService.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_2_variables.homework;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login");
+
+    }
+
+}
