011dfd2fc1661a12a2756d77ccc2e2bd7ea479b4
jelenavavere <jelenavavere@inbox.lv>
2023-10-16 22:33:10 +0300

    SumTwoNumbers

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_2_variables/homework/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_2_variables.homework;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int A = 74;
+        int B = 36;
+        int C = A + B;
+        System.out.println("Cymma " + A + " и " + B + " равна " + C);
+    }
+
+}
