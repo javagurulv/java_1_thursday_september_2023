325dec6b8221f119583ff92b4a65502fac00712f
amois <a.moisejevs@live.com>
2023-10-19 18:56:01 +0300

    Homework

---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_4/RobotApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_4;
+
+class RobotApp {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("Robo1");
+        Robot robot2 = new Robot("Robo2");
+    }
+
+}
