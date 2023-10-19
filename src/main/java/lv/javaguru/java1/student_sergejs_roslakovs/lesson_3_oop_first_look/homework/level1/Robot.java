bdfec669042b19e8c7a1a44c36ef387dcfd41fbb
disert <disert@inbox.lv>
2023-10-19 23:58:58 +0600

    homework

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_3_oop_first_look/homework/level1/Robot.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_3_oop_first_look.homework.level1;
+
+public class Robot {
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+
+
+
+
+
+
