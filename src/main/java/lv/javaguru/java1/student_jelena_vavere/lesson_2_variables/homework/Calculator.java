7fb6e3b9248e8d94320e4a8bddd2eed6d506d095
jelenavavere <jelenavavere@inbox.lv>
2023-10-22 21:16:15 +0300

    Calculator

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_2_variables/homework/Calculator.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_2_variables.homework;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 1;
+
+        for (int i = 1; i <= 10; i++){
+            int result = a * i;
+            System.out.println(a + " x " + i + " = " + result);
+        }
+
+    }
+}
