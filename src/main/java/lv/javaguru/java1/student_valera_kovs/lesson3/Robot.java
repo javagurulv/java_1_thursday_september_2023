3494f45180e075024b07b2f2ee8999f23a4e289a
Valerijs <valerka1176@gmail.com>
2023-10-24 20:44:58 +0300

    homeworkRobot

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson3/Robot.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_valera_kovs.lesson3;
+
+public class Robot {
+
+    public String name;
+
+    public Robot(String Name) {
+    }
+}
+
+
+
+
---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson3/robot2.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_valera_kovs.lesson3;
+
+public class robot2 {
+    public static void main(String[] args) {
+        Robot Andrew = new Robot("Andrew");
+        Robot Alex = new Robot("Alex");
+        System.out.println("Robot1 =" + Andrew.name);
+        System.out.println("Robot2 =" + Alex.name);
+    }
+}
