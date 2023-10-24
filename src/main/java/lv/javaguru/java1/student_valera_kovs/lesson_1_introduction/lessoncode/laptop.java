fb3944d4be706ec7077fb3a2ec02fa52b467f338
Valerijs <valerka1176@gmail.com>
2023-10-24 19:40:55 +0300

    Laptop

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/laptop.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.lessoncode;
+
+class Laptop {
+
+    private String model;
+    private int price;
+
+    public Laptop(String model) {
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson_1_introduction/lessoncode/laptop2.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_valera_kovs.lesson_1_introduction.lessoncode;
+
+public class laptop2 {
+     private static Object price;
+     private static Object model;
+     public static void main(String[] args) {
+       Laptop laptop1= new Laptop("windows");
+
+
+     }
+}
