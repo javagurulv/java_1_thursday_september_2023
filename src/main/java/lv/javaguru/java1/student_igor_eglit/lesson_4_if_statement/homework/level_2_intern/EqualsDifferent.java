49b13a1ecb8dd3ff9fc7d23e113207991901c9ed
Игорь Эглит <igeglits@gmail.com>
2023-10-26 14:36:11 +0300

    lesson 4 homework 2 равно/не равно

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_4_if_statement/homework/level_2_intern/EqualsDifferent.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_4_if_statement.homework.level_2_intern;
+
+import java.util.Scanner;
+
+public class EqualsDifferent {
+    public static void main(String[] args) {
+        System.out.println("Введите два целых числа: ");
+        var a = new Scanner(System.in).nextInt();
+        var b = new Scanner(System.in).nextInt();
+        if (a != b) {
+            System.out.println("Числа не равны");
+        } else {
+            System.out.println("Числа равны");
+        }
+    }
+}
