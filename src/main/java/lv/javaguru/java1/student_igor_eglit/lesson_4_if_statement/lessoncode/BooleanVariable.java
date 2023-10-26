d8860a88a8e7207c6d7ead4a42bd1ab7620f5db0
Игорь Эглит <igeglits@gmail.com>
2023-10-26 21:52:58 +0300

    lessoncode 4

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_4_if_statement/lessoncode/BooleanVariable.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_4_if_statement.lessoncode;
+
+public class BooleanVariable {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+    }
+}
