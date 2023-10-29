bbc2646244df7d646f8e51fe6ff698f4b7185e8f
Игорь Эглит <igeglits@gmail.com>
2023-10-29 17:45:48 +0200

    Lesson 4 homework level 4 task 1 Расчёт комиссии за использование кредитной карты. время на разработку 15 минут.

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_4_if_statement/homework/level_5_middle/MoneyWithdrawal.java
@@ -0,0 +1,23 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_4_if_statement.homework.level_5_middle;
+
+import java.util.Scanner;
+
+public class MoneyWithdrawal {
+    public static void main(String[] args) {
+        System.out.print("Введите сумму вывода наличности: ");
+        var moneyWithdrawal = new Scanner(System.in).nextInt();
+        if (moneyWithdrawal < 1000 && moneyWithdrawal > 0) {
+            var commissionResult = (moneyWithdrawal * 3.0) / 100;
+            System.out.println("комиссия за использование кредитной карты составит: " + commissionResult);
+        } else if (moneyWithdrawal >= 1000 && moneyWithdrawal < 5000) {
+            var commissionResult = (moneyWithdrawal * 4) / 100;
+            System.out.println("комиссия за использование кредитной карты составит: " + commissionResult);
+        } else if (moneyWithdrawal >= 5000) {
+            var commissionResult = (moneyWithdrawal * 5) / 100;
+            System.out.println("комиссия за использование кредитной карты составит: " + commissionResult);
+        } else {
+            System.out.println("Сумма должна быть больше нуля");
+        }
+    }
+
+}
