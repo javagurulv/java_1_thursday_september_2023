f27d3615e95451d23917e068d2a84f9c0f35d783
Valerijs <valerka1176@gmail.com>
2023-10-29 20:13:15 +0200

    boolean

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson4/booleanVariable.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_valera_kovs.lesson4;
+
+public class booleanVariable {
+
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+    }
+}
