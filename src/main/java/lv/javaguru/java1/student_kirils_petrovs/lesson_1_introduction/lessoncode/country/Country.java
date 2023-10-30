cb0194c78658fa6188fc0d6427cd2855f7ec2081
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-10-30 23:23:43 +0200

    Home Work

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_1_introduction/lessoncode/country/Country.java
@@ -0,0 +1,35 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_1_introduction.lessoncode.country;
+class Country {
+
+
+
+
+        private String countryName;
+        private String coudeCountr;
+        private int population;
+
+
+        public Country(String countryName, String coudeCountry, int population) {
+            this.countryName = countryName;
+            this.coudeCountr = coudeCountry;
+            this.population = population;
+        }
+
+        public String getCountryName() {
+            return countryName;
+        }
+
+        public String getCoudeCountr() {
+            return coudeCountr;
+        }
+
+        public int getPopulation() {
+            return population;
+        }
+    }
+
+
+
+
+
+
