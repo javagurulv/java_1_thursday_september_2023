23a3a11a1fd91ab28c74fc3ecef9dd5dfcdb9a34
Valerijs <valerka1176@gmail.com>
2023-10-30 22:05:00 +0200

    homework_3

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson4/homework_lesson_4/lvl2/homework3.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.student_valera_kovs.lesson4.homework_lesson_4.lvl2;
+
+import java.util.Scanner;
+
+public class homework3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 == num2) {
+            System.out.println("Numbers are equals");
+
+            if (num1 != num2);
+            System.out.println("Numbers are different");
+        }
+    }
+}
