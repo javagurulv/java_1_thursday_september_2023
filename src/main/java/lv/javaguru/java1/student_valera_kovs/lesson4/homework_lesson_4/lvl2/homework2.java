afcb418405a34b8f14ed326f88a29a9e01a61c03
Valerijs <valerka1176@gmail.com>
2023-10-30 21:53:09 +0200

    homework_2

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson4/homework_lesson_4/lvl2/homework2.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_valera_kovs.lesson4.homework_lesson_4.lvl2;
+
+import java.util.Scanner;
+
+public class homework2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: num1 ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number: num2 ");
+        int num2 = scanner.nextInt();
+        if (num1 < num2) {
+            System.out.println("num1 is less");
+        } else {
+            System.out.println("num2 is less");
+        }
+    }
+}
