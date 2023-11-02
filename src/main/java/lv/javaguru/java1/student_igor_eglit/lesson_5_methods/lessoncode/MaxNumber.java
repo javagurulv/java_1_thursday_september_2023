409284905e64763ea6cade6dee3402063c4de5c5
Игорь Эглит <igeglits@gmail.com>
2023-11-02 20:53:49 +0200

    больше меньше метод

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_5_methods/lessoncode/MaxNumber.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_5_methods.lessoncode;
+
+class MaxNumber {
+    int findMax(int number1, int number2) {
+        return number1 > number2 ? number1 : number2;
+    }
+}
+
---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_5_methods/lessoncode/MaxNumberApplication.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_5_methods.lessoncode;
+
+public class MaxNumberApplication {
+    public static void main(String[] args) {
+        MaxNumber maxNumber=new MaxNumber();
+        System.out.println("2 and 3 = "+maxNumber.findMax(2,3));
+        System.out.println("8 and 3 = "+maxNumber.findMax(8,3));
+        System.out.println("8 and 11 = "+maxNumber.findMax(8,11));
+    }
+}
