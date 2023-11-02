fa02b01f6da4be215d1e3c45a6d8743f756e349b
javagurulv <javagurulv@gmail.com>
2023-11-02 20:28:36 +0200

    lesson 5

--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/Calculator.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/Calculator.java
@@ -2,6 +2,16 @@ package lv.javaguru.java1.teacher.lesson_5_methods.lessoncode;
 
 class Calculator {
 
+    double findAverageDouble(int number1, int number2) {
+        double average = (number1 + number2) / 2.0;
+        return average;
+    }
+
+    int findAverage(int number1, int number2) {
+       int average = (number1 + number2) / 2;
+       return average;
+    }
+
     boolean isEven(int number) {
         if (number % 2 == 0) {
             return true;
--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/CalculatorApplication.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/CalculatorApplication.java
@@ -15,6 +15,13 @@ class CalculatorApplication {
         System.out.println("6 = " + calculator.isOdd(6));
         System.out.println("7 = " + calculator.isOdd(7));
 
+        System.out.println("4-4 = " + calculator.findAverage(4, 4));
+        System.out.println("5-4 = " + calculator.findAverage(5, 4));
+        System.out.println("6-8 = " + calculator.findAverage(6, 8));
+        System.out.println("7-9 = " + calculator.findAverage(7, 9));
+
+        System.out.println("5-4 = " + calculator.findAverageDouble(5, 4));
+
     }
 
 }
