fe7748f608169863b3c3f88630a62976d0ecdb71
javagurulv <javagurulv@gmail.com>
2023-11-02 21:17:39 +0200

    lesson 5

--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumberApplication.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumberApplication.java
@@ -8,6 +8,11 @@ class MaxNumberApplication {
         System.out.println("Max(2,2) = " + maxNumber.findMax(2,2));
         System.out.println("Max(2,3) = " + maxNumber.findMax(2,3));
         System.out.println("Max(5,3) = " + maxNumber.findMax(5,3));
+
+        System.out.println("Max(5,3,7) = " + maxNumber.findMax(5,3,7));
+        System.out.println("Max(10,3,7) = " + maxNumber.findMax(10,3,7));
+        System.out.println("Max(5,9,7) = " + maxNumber.findMax(5,9,7));
+
     }
 
 }
