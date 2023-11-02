f22d8b22235f8562b943fc50992c2ad05e1cc922
disert <disert@inbox.lv>
2023-11-02 23:31:04 +0600

    Homework

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_5_methods/Calculator.java
@@ -0,0 +1,39 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_5_methods;
+
+public class Calculator {
+    double findAverageDouble(int number1, int number2) {
+        double average = (number1 + number2) / 2.0;
+        return average;
+    }
+
+    int findAverage(int number1, int number2) {
+        int average = (number1 + number2) / 2;
+        return average;
+    }
+
+    boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isOdd(int number) {
+        if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
