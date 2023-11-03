e775221fb6e150177f045f1a4495b572225cd8c0
Игорь Эглит <igeglits@gmail.com>
2023-11-03 16:26:26 +0200

    lesson 5 HW lvl 2 task 1

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_5_methods/homework/level_2_intern/LeapYearApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_5_methods.homework.level_2_intern;
+
+public class LeapYearApp {
+    public static void main(String[] args) {
+        LeapYear findOutLeap = new LeapYear();
+        System.out.println(findOutLeap.year(2020));
+        System.out.println(findOutLeap.year(1924));
+        System.out.println(findOutLeap.year(1978));
+        System.out.println(findOutLeap.year(2001));
+        System.out.println(findOutLeap.year(2005));
+
+    }
+}
