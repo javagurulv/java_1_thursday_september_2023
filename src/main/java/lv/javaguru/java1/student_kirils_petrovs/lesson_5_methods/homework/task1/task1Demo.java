ab7be474560bcaebd4c82d21cfa8bf2827b026ff
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-11-04 22:16:35 +0200

    lesson 5 lv 1 task1

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_5_methods/homework/task1/task1Demo.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_5_methods.homework.task1;
+
+class task1Demo {
+
+    public static void main(String[] args) {
+        task1 fizzBuzz = new task1 ();
+        System.out.println("Number 5 = " + fizzBuzz.detect(5));
+        System.out.println("Number 6 = " + fizzBuzz.detect(6));
+        System.out.println("Number 10 = " + fizzBuzz.detect(10));
+        System.out.println("Number 15 = " + fizzBuzz.detect(15));
+    }
+
+}
