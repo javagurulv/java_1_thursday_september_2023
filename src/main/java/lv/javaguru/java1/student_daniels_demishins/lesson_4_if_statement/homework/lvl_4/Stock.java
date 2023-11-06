7e3412f2629be23c8e027f6dfa675d2fd4a9688b
Wanxezar <zenadik@inbox.lv>
2023-11-06 15:44:33 +0200

    Lesson 4 HW lvl4 - lvl6

--- src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_4_if_statement/homework/lvl_4/Stock.java
+++ src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_4_if_statement/homework/lvl_4/Stock.java
@@ -1,36 +1,43 @@
 package lv.javaguru.java1.student_daniels_demishins.lesson_4_if_statement.homework.lvl_4;
 
- class Stock {
+class Stock {
 
-     private String company;
-     private int currentPrice;
-     private int minPrice;
-     private int maxPrice;
+    private String company;
+    private int currentPrice;
+    private int minPrice;
+    private int maxPrice;
 
-     public Stock(String company, int currentPrice, int minPrice, int maxPrice) {
-         this.company = company;
-         this.currentPrice = currentPrice;
-         this.minPrice = minPrice;
-         this.maxPrice = maxPrice;
-     }
+    public Stock(String company, int currentPrice) {
+        this.company = company;
+        this.currentPrice = currentPrice;
+        this.minPrice = currentPrice;
+        this.maxPrice = currentPrice;
+    }
 
-     public String getCompany() {
-         return company;
-     }
+    void updatePrice(int newPrice) {
+        this.currentPrice = newPrice;
+        if (currentPrice < minPrice) {
+            minPrice =currentPrice;
+        } else if (currentPrice > maxPrice) {
+            maxPrice =currentPrice;
+        }
+    }
 
-     public int getCurrentPrice() {
-         return currentPrice;
-     }
+    public String getCompany() {
+        return company;
+    }
 
-     public int getMinPrice() {
-         return minPrice;
-     }
+    public int getCurrentPrice() {
+        return currentPrice;
+    }
 
-     public int getMaxPrice() {
-         return maxPrice;
-     }
+    public int getMinPrice() {
+        return minPrice;
+    }
 
-     void updatePrice(int newPrice){
-         
-     }
- }
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_4_if_statement/homework/lvl_6/YearSeasons.java
@@ -0,0 +1,52 @@
+package lv.javaguru.java1.student_daniels_demishins.lesson_4_if_statement.homework.lvl_6;
+
+import java.util.Scanner;
+
+class YearSeasons {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number 1 - 12: ");
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Winter");
+                break;
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+                System.out.println("Spring");
+                break;
+            case 4:
+                System.out.println("Spring");
+                break;
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+                System.out.println("Summer");
+                break;
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+                System.out.println("Autumn");
+                break;
+            case 10:
+                System.out.println("Autumn");
+                break;
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Please enter 1-12 ");
+        }
+    }
+}
