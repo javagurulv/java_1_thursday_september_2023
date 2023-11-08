3f056875055f0149fe1a5417c799ca4e088bbaa2
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-11-08 17:22:47 +0200

    leap year

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_5_methods/homework/level_3_junior/LeapYear.java
@@ -0,0 +1,29 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_5_methods.homework.level_3_junior;
+
+class LeapYear {
+    public void inputUser(int input) {
+
+
+        if (input % 4 == 0 ) {
+            System.out.println("leap year");
+        } else {
+            System.out.println("common year");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
