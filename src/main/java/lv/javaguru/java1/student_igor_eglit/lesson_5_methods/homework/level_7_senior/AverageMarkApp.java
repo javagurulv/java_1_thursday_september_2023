252af1b59555121b08aedd4b71a210f1468b9cbc
Игорь Эглит <igeglits@gmail.com>
2023-11-09 12:18:42 +0200

    lesson 5 HW lvl 7 task 3

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_5_methods/homework/level_7_senior/AverageMarkApp.java
@@ -0,0 +1,100 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_5_methods.homework.level_7_senior;
+
+import java.util.Scanner;
+
+class AverageMarkApp {
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество студентов: ");
+        int studentCount = Integer.parseInt(scanner.nextLine());
+
+        String[] students = new String[studentCount];
+
+        studentNames(studentCount, scanner, students);
+
+        double[] grades = new double[studentCount];
+
+        studentGrades(studentCount, students, scanner, grades);
+
+        var average = getAverage(grades, studentCount);
+
+        System.out.println("Студенты со средним баллом выше среднего:");
+        for (int i = 0; i < studentCount; i++) {
+            if (grades[i] > average) {
+                System.out.println(students[i] + ": " + grades[i]);
+            }
+        }
+
+        System.out.println("Программа завершена.");
+    }
+
+    private static double getAverage(double[] grades, int studentCount) {
+        double sum = 0.0;
+        for (double grade : grades) {
+            sum += grade;
+        }
+
+        double average = sum / studentCount;
+        System.out.println("Средний балл: " + average);
+        return average;
+    }
+
+    private static void studentGrades(int studentCount, String[] students, Scanner scanner, double[] grades) {
+        for (int i = 0; i < studentCount; i++) {
+            System.out.print("Введите оценку для студента " + students[i] + ": ");
+            double grade = Double.parseDouble(scanner.nextLine());
+            grades[i] = grade;
+        }
+    }
+
+    private static void studentNames(int studentCount, Scanner scanner, String[] students) {
+        for (int i = 0; i < studentCount; i++) {
+            System.out.print("Введите имя студента " + (i + 1) + ": ");
+            String studentName = scanner.nextLine();
+            students[i] = studentName;
+        }
+    }
+}
+/*
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество студентов: ");
+        int studentCount = Integer.parseInt(scanner.nextLine());
+
+        String[] students = new String[studentCount];
+
+        for (int i = 0; i < studentCount; i++) {
+            System.out.print("Введите имя студента " + (i + 1) + ": ");
+            String studentName = scanner.nextLine();
+            students[i] = studentName;
+        }
+
+        double[] grades = new double[studentCount];
+
+        for (int i = 0; i < studentCount; i++) {
+            System.out.print("Введите оценку для студента " + students[i] + ": ");
+            double grade = Double.parseDouble(scanner.nextLine());
+            grades[i] = grade;
+        }
+
+        double sum = 0.0;
+        for (double grade : grades) {
+            sum += grade;
+        }
+
+        double average = sum / studentCount;
+        System.out.println("Средний балл: " + average);
+
+        System.out.println("Студенты со средним баллом выше среднего:");
+        for (int i = 0; i < studentCount; i++) {
+            if (grades[i] > average) {
+                System.out.println(students[i] + ": " + grades[i]);
+            }
+        }
+
+        System.out.println("Программа завершена.");
+    }
+ */
---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_5_methods/homework/level_7_senior/StockApp.java
@@ -0,0 +1,111 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_5_methods.homework.level_7_senior;
+
+import java.util.Scanner;
+
+class StockApp {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введите название акции: ");
+            String stockName = scanner.nextLine();
+
+            System.out.print("Введите количество котировок акции: ");
+            int stockPriceCount = Integer.parseInt(scanner.nextLine());
+
+            var prices = getPrices(stockPriceCount, scanner);
+
+            var maxPrice = getMaxPrice(prices);
+            System.out.println("Максимальная цена акции " + stockName + ": " + maxPrice);
+
+            var minPrice = getMinPrice(prices);
+            System.out.println("Минимальная цена акции " + stockName + ": " + minPrice);
+
+            var totalPrice = getTotalPrice(prices);
+            double averagePrice = totalPrice / prices.length;
+            System.out.println("Средняя цена акции " + stockName + ": " + averagePrice);
+
+            System.out.println("Программа завершена.");
+        }
+
+    private static double getTotalPrice(double[] prices) {
+        double totalPrice = 0.0;
+        for (double price : prices) {
+            totalPrice = totalPrice + price;
+        }
+        return totalPrice;
+    }
+
+    private static double getMinPrice(double[] prices) {
+        double minPrice = prices[0];
+        for (double price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+        }
+        return minPrice;
+    }
+
+    private static double getMaxPrice(double[] prices) {
+        double maxPrice = prices[0];
+        for (double price : prices) {
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+        }
+        return maxPrice;
+    }
+
+    private static double[] getPrices(int stockPriceCount, Scanner scanner) {
+        double[] prices = new double[stockPriceCount];
+
+        for (int i = 0; i < stockPriceCount; i++) {
+            System.out.print("Введите цену акции " + (i + 1) + ": ");
+            prices[i] = Double.parseDouble(scanner.nextLine());
+        }
+        return prices;
+    }
+}
+/*
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите название акции: ");
+        String stockName = scanner.nextLine();
+
+        System.out.print("Введите количество котировок акции: ");
+        int stockPriceCount = Integer.parseInt(scanner.nextLine());
+
+        double[] prices = new double[stockPriceCount];
+
+        for (int i = 0; i < stockPriceCount; i++) {
+            System.out.print("Введите цену акции " + (i + 1) + ": ");
+            prices[i] = Double.parseDouble(scanner.nextLine());
+        }
+
+        double maxPrice = prices[0];
+        for (double price : prices) {
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+        }
+        System.out.println("Максимальная цена акции " + stockName + ": " + maxPrice);
+
+        double minPrice = prices[0];
+        for (double price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+        }
+        System.out.println("Минимальная цена акции " + stockName + ": " + minPrice);
+
+        double totalPrice = 0.0;
+        for (double price : prices) {
+            totalPrice = totalPrice + price;
+        }
+        double averagePrice = totalPrice / prices.length;
+        System.out.println("Средняя цена акции " + stockName + ": " + averagePrice);
+
+        System.out.println("Программа завершена.");
+    }
+
+ */
