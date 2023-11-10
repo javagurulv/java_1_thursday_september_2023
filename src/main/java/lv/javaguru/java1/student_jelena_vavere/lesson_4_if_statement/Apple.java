e67c69a551de2897b55fa71fb9d4909a0baf81a4
jelenavavere <jelenavavere@inbox.lv>
2023-11-10 16:31:00 +0200

    Apple

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_4_if_statement/Apple.java
@@ -0,0 +1,29 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_4_if_statement;
+
+
+class Apple {
+
+    private String type;
+    private String color;
+    private int weight;
+
+    public Apple(String type, String color, int weight) {
+        this.type = type;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+}
+
