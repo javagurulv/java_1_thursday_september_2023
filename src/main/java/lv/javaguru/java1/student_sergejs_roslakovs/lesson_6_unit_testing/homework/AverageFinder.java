9254eae1023bde9da0897d4db206284d6702f275
disert <disert@inbox.lv>
2023-11-13 13:04:20 +0600

    Homework

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_6_unit_testing/homework/AverageFinder.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_6_unit_testing.homework;
+
+class AverageFinder {
+    public double findAverage(int num1, int num2) {
+        double sum = num1 + num2;
+        return sum / 2.0;
+    }
+
+
+
+
+}
