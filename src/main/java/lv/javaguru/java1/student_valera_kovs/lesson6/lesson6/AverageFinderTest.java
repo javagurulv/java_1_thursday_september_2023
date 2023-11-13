2f339dc0345fa75446090049b65c4d8e5f725ae9
Valerijs <valerka1176@gmail.com>
2023-11-13 21:04:06 +0200

    homework 1

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson6/lesson6/AverageFinderTest.java
@@ -0,0 +1,35 @@
+package lv.javaguru.java1.student_valera_kovs.lesson6.lesson6;
+
+import lv.javaguru.java1.teacher.lesson_6_unit_testing.homework.level_1_intern.AverageFinder;
+
+public class AverageFinderTest {
+
+    public static void main(String[] args) {
+        AverageFinderTest averageFinderTest = new AverageFinderTest();
+        averageFinderTest.testCase1();
+        averageFinderTest.testCase2();
+
+    }
+    public void testCase1(){
+        AverageFinder averageFinder = new AverageFinder();
+        int testResult = averageFinder.findAverage(2,6);
+        if (testResult==4) {
+            System.out.println("TestCase1 OK");
+        }else {
+            System.out.println("TestCase1 Fail");
+            System.out.println(testResult +": result 2.6");
+        }
+
+    }
+    public void testCase2(){
+        AverageFinder averageFinder = new AverageFinder();
+        int testResult = averageFinder.findAverage(6,9);
+        if (testResult==7) {
+            System.out.println("TestCase2 OK");
+        }else {
+            System.out.println("TestCase2 Fail");
+            System.out.println(testResult +": result 7");
+        }
+
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson6/lesson6/AverandeFinder.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_valera_kovs.lesson6.lesson6;
+
+class AverandeFinder {
+
+    public int findAverage(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum / 2;
+    }
+
+}
