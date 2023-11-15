4bd0096ed77c70d4d01c825aaffb3bc0a0edbe6d
disert <disert@inbox.lv>
2023-11-15 22:10:24 +0600

    Homework

--- src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_6_unit_testing/homework/level5/ColorWaveDetectorTest.java
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_6_unit_testing/homework/level5/ColorWaveDetectorTest.java
@@ -28,7 +28,7 @@ public class ColorWaveDetectorTest {
     public void n2() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(470);
-        if (result.equals("Violet")) {
+        if (result.equals("Blue")) {
             System.out.println("Wave: " + 470 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 470 + " " + result + " - OK");
@@ -37,7 +37,7 @@ public class ColorWaveDetectorTest {
     public void n3() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(550);
-        if (result.equals("Violet")) {
+        if (result.equals("Green")) {
             System.out.println("Wave: " + 550 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 550 + " " + result + " - OK");
@@ -46,7 +46,7 @@ public class ColorWaveDetectorTest {
     public void n4() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(580);
-        if (result.equals("Violet")) {
+        if (result.equals("Yellow")) {
             System.out.println("Wave: " + 580 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 580 + " " + result + " - OK");
@@ -55,7 +55,7 @@ public class ColorWaveDetectorTest {
     public void n5() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(600);
-        if (result.equals("Violet")) {
+        if (result.equals("Orange")) {
             System.out.println("Wave: " + 600 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 600 + " " + result + " - OK");
@@ -64,7 +64,7 @@ public class ColorWaveDetectorTest {
     public void n6() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(650);
-        if (result.equals("Violet")) {
+        if (result.equals("Red")) {
             System.out.println("Wave: " + 650 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 650 + " " + result + " - OK");
@@ -73,7 +73,7 @@ public class ColorWaveDetectorTest {
     public void n7() {
         ColorWaveDetector colorWaveDetector = new ColorWaveDetector();
         String result = colorWaveDetector.colorWave(100);
-        if (result.equals("Violet")) {
+        if (result.equals("Invisible Light")) {
             System.out.println("Wave: " + 100 + " " + result + " - OK");
         } else {
             System.out.println("Wave: " + 100 + " " + result + " - OK");
