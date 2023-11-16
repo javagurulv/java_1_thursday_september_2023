28d2479d25d2421e40f711107ec414fd063465a0
javagurulv <javagurulv@gmail.com>
2023-11-16 19:34:59 +0200

    lesson_7 array for

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_array_for/lessoncode/tasks/PrintNumbers.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.teacher.lesson_7_array_for.lessoncode.tasks;
+
+class PrintNumbers {
+
+    public void printNumbers() {
+        // i = i + 1
+        // i++
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_array_for/lessoncode/tasks/PrintNumbersApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.teacher.lesson_7_array_for.lessoncode.tasks;
+
+class PrintNumbersApp {
+
+    public static void main(String[] args) {
+        PrintNumbers printNumbers = new PrintNumbers();
+        printNumbers.printNumbers();
+    }
+
+}
