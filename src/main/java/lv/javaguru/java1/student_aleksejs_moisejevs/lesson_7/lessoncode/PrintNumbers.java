3d9df00b7e6f88fe6eddbe0a2bc64430d81d608e
amois <a.moisejevs@live.com>
2023-11-16 19:50:01 +0200

    LessonCodeArray

---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_7/lessoncode/PrintNumbers.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_7.lessoncode;
+
+import lv.javaguru.java1.student_daniels_demishins.lesson_2_variables.homework.lvl_4.SystemOutPrint;
+
+public class PrintNumbers {
+        public void printNumbers() {
+            System.out.println(1 + " " + 2 + " " + 3 +" ");
+
+
+
+            for (int i = 1; i <= 10; i++ ) {
+                System.out.println(i);
+                System.out.println(" ");
+
+
+            }
+            System.out.println ("Finish");
+        }
+
+}
+
---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_7/lessoncode/PrintNumbersApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_7.lessoncode;
+
+class PrintNumbersApp {
+
+    public static void main(String[] args) {
+        PrintNumbers printNumbers = new PrintNumbers();
+        printNumbers.printNumbers();
+    }
+
+}
