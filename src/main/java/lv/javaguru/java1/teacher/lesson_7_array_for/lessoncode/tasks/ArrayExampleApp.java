4c597ded85d77a2faa8720bd4dd8553bab1898b2
javagurulv <javagurulv@gmail.com>
2023-11-16 21:09:42 +0200

    lesson_7 array for

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_array_for/lessoncode/tasks/ArrayExampleApp.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.teacher.lesson_7_array_for.lessoncode.tasks;
+
+class ArrayExampleApp {
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        int[] array = arrayExample.createArray();
+
+        array[1] = 9;
+        array[3] = 7;
+        array[5] = 3;
+        array[7] = 8;
+        array[9] = 7;
+
+        int sum = arrayExample.calculateSum(array);
+        System.out.println("Sum = " + sum);
+
+        double average = arrayExample.calculateAverage(sum, array.length);
+        System.out.println("Average = " + average);
+    }
+
+}
