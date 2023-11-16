5139300842e6262fb7c65abd012d865dac299c45
amois <a.moisejevs@live.com>
2023-11-16 20:20:35 +0200

    LessonCode

---
+++ src/main/java/lv/javaguru/java1/student_aleksejs_moisejevs/lesson_7/lessoncode/SumCalculatorApp.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_aleksejs_moisejevs.lesson_7.lessoncode;
+
+public class SumCalculatorApp {
+
+    public static void main(String[] args) {
+        SumCalculator calculator = new SumCalculator();
+        int sum = calculator.calculateSum(10, 90);
+        System.out.println(sum);
+    }
+
+}
