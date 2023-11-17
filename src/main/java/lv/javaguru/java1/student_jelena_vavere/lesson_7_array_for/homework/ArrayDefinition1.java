df5de55ea454877e1929dc22fe23f4e4f2c63eb3
jelenavavere <jelenavavere@inbox.lv>
2023-11-17 23:20:15 +0200

    ArrayDefinition1

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/homework/ArrayDefinition1.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.homework;
+
+public class ArrayDefinition1 {
+    public static void main(String[] args) {
+        int[]numbers = new int[10];
+    }
+
+}
+
+
