76eca6d085bcbb56cf45afe45ac9d48f3d6e7519
Игорь Эглит <igeglits@gmail.com>
2023-11-17 15:27:15 +0200

    lesson 7 HW Level 1

---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_7_array_for/homework/level_1_intern/ArrayDefinition1.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_7_array_for.homework.level_1_intern;
+
+class ArrayDefinition1 {
+
+
+    public static void main(String[] args) {
+        int [] numbers = new int[10];
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_7_array_for/homework/level_1_intern/ArrayLength2.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_7_array_for.homework.level_1_intern;
+
+class ArrayLength2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        numbers[0]=1;
+        numbers[1]=2;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+}
