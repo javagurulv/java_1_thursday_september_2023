d5b92e406b6fde8d5b7bfa8f121e775e78fadbdb
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-11-17 20:50:53 +0200

    level_1_intern

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_7_array_for/homework/level_1_intern/task_2/ArrayDefinition2.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_7_array_for.homework.level_1_intern.task_2;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte [] numbers = new byte[10];
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_7_array_for/homework/level_1_intern/task_6/ArrayLength1.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_7_array_for.homework.level_1_intern.task_6;
+
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+        System.out.println(numbers [0]);
+    }
+
+}
