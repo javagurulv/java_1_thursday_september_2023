7023f704b217cc39d647798f8362e34d6cc0dbca
jelenavavere <jelenavavere@inbox.lv>
2023-11-17 22:14:48 +0200

    PrintNumbers

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/lessoncode/PrintNumbers.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.lessoncode;
+
+class PrintNumbers {
+    public void printNumbers() {
+        // i = i + 1
+        // i++
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println("Finish");
+    }
+
+}
+
---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/lessoncode/PrintNumbersApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.lessoncode;
+class PrintNumbersApp {
+
+
+        public static void main(String[] args) {
+            PrintNumbers printNumbers = new PrintNumbers();
+            printNumbers.printNumbers();
+        }
+
+    }
