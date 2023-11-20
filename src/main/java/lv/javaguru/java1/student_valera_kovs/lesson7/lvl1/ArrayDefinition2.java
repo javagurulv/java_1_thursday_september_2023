40ddb2f55c778e40e850e43f5e4524fb914e38a9
Valerijs <valerka1176@gmail.com>
2023-11-20 17:33:08 +0200

    homework 1

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson7/lvl1/ArrayDefinition2.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_valera_kovs.lesson7.lvl1;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte [] numbers = new byte[10];
+    }
+
+}
+
