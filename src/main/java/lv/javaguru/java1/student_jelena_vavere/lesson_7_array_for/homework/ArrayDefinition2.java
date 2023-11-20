5a67ec16e025d31cc7f6270b298f0a6a8f6bbdaa
jelenavavere <jelenavavere@inbox.lv>
2023-11-20 21:01:03 +0200

    ArrayDefinition2

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/homework/ArrayDefinition2.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.homework;
+
+public class ArrayDefinition2 {
+    public static void main(String[] args) {
+        byte [] numbers = new byte[10];
+    }
+
+}
+
+
