134c546f58e190035355d070e10bd536d24f2000
jelenavavere <jelenavavere@inbox.lv>
2023-11-20 21:19:13 +0200

    ArrayDefinition3

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/homework/ArrayDefinition3.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.homework;
+ class ArrayDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 3};
+    }
+
+}
+
+
