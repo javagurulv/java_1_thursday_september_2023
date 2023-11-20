0f86985adaf2f883d79db944274bf0025292e68b
Valerijs <valerka1176@gmail.com>
2023-11-20 18:12:52 +0200

    homework 3

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson7/lvl1/ArrayLength1.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_valera_kovs.lesson7.lvl1;
+
+public class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+      int number =0;
+        System.out.println(numbers[0]);
+    }
+
+}
+
