617d650d672af3d89f864432861616e9e4f633d8
javagurulv <javagurulv@gmail.com>
2023-11-21 14:52:54 +0200

    lesson_8 and 9

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_8_project_school_dairy_part_1/lessoncode/Mark.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.teacher.lesson_8_project_school_dairy_part_1.lessoncode;
+
+class Mark {
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
