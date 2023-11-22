478d385ab3e7c91fb6f6545b46176f55e9a47ac5
Ersillus <146378524+Ersillus@users.noreply.github.com>
2023-11-22 14:52:21 +0200

    level_2_intern

---
+++ src/main/java/lv/javaguru/java1/student_kirils_petrovs/lesson_7_array_for/homework/level_2_intern/task_6/ForLoopDefinition6.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_kirils_petrovs.lesson_7_array_for.homework.level_2_intern.task_6;
+
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
+
