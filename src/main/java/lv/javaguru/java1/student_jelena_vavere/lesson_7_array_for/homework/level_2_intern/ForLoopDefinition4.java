f7f69fc4ddc5a57647b5b8598510343ded29abd6
jelenavavere <jelenavavere@inbox.lv>
2023-11-22 21:11:18 +0200

    ForLoopDefinition4

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/homework/level_2_intern/ForLoopDefinition4.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.homework.level_2_intern;
+
+public class ForLoopDefinition4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+
