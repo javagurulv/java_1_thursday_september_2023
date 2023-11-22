145fbd1dff8ec27a05b6dc8401a3d14cd72c5341
jelenavavere <jelenavavere@inbox.lv>
2023-11-22 20:23:52 +0200

    ArrayLength1

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_7_array_for/homework/level_1_intern/ArrayLength1.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_7_array_for.homework.level_1_intern;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+        System.out.println(numbers[0]);
+    }
+
+}
+
