08f74166cea46494c3fd3fe6a74fb12c3cc8c8ab
Даниэль <zenadik@inbox.lv>
2023-11-22 18:21:07 +0200

    Lesson 7 Lessoncode

---
+++ src/main/java/lv/javaguru/java1/student_daniels_demishins/lesson_7_array_for/lessoncode/OneToHundred.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_daniels_demishins.lesson_7_array_for.lessoncode;
+
+class OneToHundred {
+    public int calculateSum(int start, int end) {
+        int sum = 0;
+        for (int i = start; i<=end; i++){
+            sum = sum + i;
+
+        }  return sum;
+    }
+}
