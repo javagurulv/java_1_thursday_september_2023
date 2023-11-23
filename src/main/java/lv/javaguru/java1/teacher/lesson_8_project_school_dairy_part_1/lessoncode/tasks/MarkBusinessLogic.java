c21677d37c48d7242e0302110ae7bdad4767892c
javagurulv <javagurulv@gmail.com>
2023-11-23 21:47:02 +0200

    Mark code

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_8_project_school_dairy_part_1/lessoncode/tasks/MarkBusinessLogic.java
@@ -0,0 +1,32 @@
+package lv.javaguru.java1.teacher.lesson_8_project_school_dairy_part_1.lessoncode.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkBusinessLogic {
+
+    private List<Mark> marks;
+
+    public MarkBusinessLogic() {
+        this.marks = new ArrayList<>();
+    }
+
+    public void addMark(String subject, int markValue) {
+        Mark mark = new Mark(subject, markValue);
+        marks.add(mark);
+    }
+
+    public int findMaxMark(String subject) {
+        int maxMark = 0;
+        for (int i = 0; i < marks.size(); i++) {
+            Mark mark = marks.get(i);
+            String markSubject = mark.getSubject();
+            if (markSubject.equals(subject)
+                && mark.getMarkValue() > maxMark) {
+                maxMark = mark.getMarkValue();
+            }
+        }
+        return maxMark;
+    }
+
+}
