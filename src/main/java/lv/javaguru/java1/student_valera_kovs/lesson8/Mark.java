05fc68c8b8a978ddaf9dd594937cb30e1a9b96d5
Valerijs <valerka1176@gmail.com>
2023-11-25 15:53:23 +0200

    homework1

---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson8/Mark.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_valera_kovs.lesson8;
+
+class Mark {
+
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_valera_kovs/lesson8/SchoolDiaryApp.java
@@ -0,0 +1,24 @@
+package lv.javaguru.java1.student_valera_kovs.lesson8;
+import java.util.List;
+public class SchoolDiaryApp {
+
+    public static void main(String[] args) {
+        SchoolDiary schoolDiary = new SchoolDiary();
+        schoolDiary.addMark(new Mark("Math", 10));
+        schoolDiary.addMark(new Mark("Eng", 5));
+        schoolDiary.addMark(new Mark("Eng", 6));
+        schoolDiary.addMark(new Mark("Math", 6));
+
+        int maxMark = schoolDiary.findMaxMark("Math");
+
+        int maxMark2 = schoolDiary.findMaxMark(
+                List.of(
+                        new Mark("Math", 10),
+                        new Mark("Math", 5)
+                )
+        );
+
+
+    }
+
+}
