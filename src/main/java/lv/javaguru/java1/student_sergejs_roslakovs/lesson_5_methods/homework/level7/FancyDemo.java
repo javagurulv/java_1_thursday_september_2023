45511c83231768e15b18ca8df8272cfffeb5d42c
disert <disert@inbox.lv>
2023-11-26 21:49:39 +0600

    Homework

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_roslakovs/lesson_5_methods/homework/level7/FancyDemo.java
@@ -0,0 +1,41 @@
+package lv.javaguru.java1.student_sergejs_roslakovs.lesson_5_methods.homework.level7;
+
+import java.lang.System;
+
+
+public class FancyDemo {
+
+    public String FancyDemo() {
+        String sTmp = "";
+        sTmp = sTmp + "                                             _..  " + System.lineSeparator();
+        sTmp = sTmp + "                                          .qd$$$$bp." + System.lineSeparator();
+        sTmp = sTmp + "                                        .q$$$$$$$$$$m." + System.lineSeparator();
+        sTmp = sTmp + "                                       .$$$$$$$$$$$$$$" + System.lineSeparator();
+        sTmp = sTmp + "                                     .q$$$$$$$$$$$$$$$$" + System.lineSeparator();
+        sTmp = sTmp + "                                    .$$$$$$$$$$$$P\\$$$$;" + System.lineSeparator();
+        sTmp = sTmp + "                                  .q$$$$$$$$$P^\"_.`;$$$$" + System.lineSeparator();
+        sTmp = sTmp + "                                 q$$$$$$$P;\\   ,  /$$$$P" + System.lineSeparator();
+        sTmp = sTmp + "                               .$$$P^::Y$/`  _  .:.$$$/" + System.lineSeparator();
+        sTmp = sTmp + "                              .P.:..    \\ `._.-:.. \\$P" + System.lineSeparator();
+        sTmp = sTmp + "                              $':.  __.. :   :..    :'" + System.lineSeparator();
+        sTmp = sTmp + "                             /:_..::.   `. .:.    .'|" + System.lineSeparator();
+        sTmp = sTmp + "                           _::..          T:..   /  :" + System.lineSeparator();
+        sTmp = sTmp + "                        .::..             J:..  :  :" + System.lineSeparator();
+        sTmp = sTmp + "                     .::..          7:..   F:.. :  ;" + System.lineSeparator();
+        sTmp = sTmp + "                 _.::..             |:..   J:.. `./" + System.lineSeparator();
+        sTmp = sTmp + "            _..:::..               /J:..    F:.  : " + System.lineSeparator();
+        sTmp = sTmp + "          .::::..                .T  \\:..   J:.  /" + System.lineSeparator();
+        sTmp = sTmp + "         /:::...               .' `.  \\:..   F_o'" + System.lineSeparator();
+        sTmp = sTmp + "        .:::...              .'     \\  \\:..  J ;" + System.lineSeparator();
+        sTmp = sTmp + "        ::::...           .-'`.    _.`._\\:..  \\'" + System.lineSeparator();
+        sTmp = sTmp + "        ':::...         .'  `._7.-'_.-  `\\:.   \\" + System.lineSeparator();
+        sTmp = sTmp + "         \\:::...   _..-'__.._/_.--' ,:.   b:.   \\._ " + System.lineSeparator();
+        sTmp = sTmp + "          `::::..-\"_.'-\"_..--\"      :..   /):.   `.\\   " + System.lineSeparator();
+        sTmp = sTmp + "            `-:/\"-7.--\"\"            _::.-'P::..    \\} " + System.lineSeparator();
+        sTmp = sTmp + " _....------\"\"\"\"\"\"            _..--\".-'   \\::..     `. " + System.lineSeparator();
+        sTmp = sTmp + "(::..              _...----\"\"\"  _.-'       `---:..    `-." + System.lineSeparator();
+        sTmp = sTmp + " \\::..      _.-\"\"\"\"   `\"\"\"\"---\"\"                `::...___)" + System.lineSeparator();
+        return sTmp + "  `\\:._.-\"\"\"                             " + System.lineSeparator();
+
+    }
+}
