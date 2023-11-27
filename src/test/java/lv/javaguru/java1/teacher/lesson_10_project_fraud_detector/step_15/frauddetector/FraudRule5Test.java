33abc05017b2057d5e6a8a3172db3a536f9e38da
javagurulv <javagurulv@gmail.com>
2023-11-27 08:56:19 +0200

    fraud detector project test code

---
+++ src/test/java/lv/javaguru/java1/teacher/lesson_10_project_fraud_detector/step_15/frauddetector/FraudRule5Test.java
@@ -0,0 +1,41 @@
+package lv.javaguru.java1.teacher.lesson_10_project_fraud_detector.step_15.frauddetector;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FraudRule5Test {
+
+    private FraudRule5 fraudRule
+            = new FraudRule5();
+
+    @Test
+    public void shouldBeFraud() {
+        Trader trader = new Trader("Zorro", "Berlin", "Germany");
+        Transaction transaction = new Transaction(trader, 1001);
+        assertTrue(fraudRule.isFraud(transaction));
+    }
+
+    @Test
+    public void shouldNotBeFraud_1000() {
+        Trader trader = new Trader("Zorro", "Berlin", "Germany");
+        Transaction transaction = new Transaction(trader, 1000);
+        assertFalse(fraudRule.isFraud(transaction));
+    }
+
+    @Test
+    public void shouldNotBeFraud_LessThan_1000() {
+        Trader trader = new Trader("Zorro", "Berlin", "Germany");
+        Transaction transaction = new Transaction(trader, 10);
+        assertFalse(fraudRule.isFraud(transaction));
+    }
+
+    @Test
+    public void shouldNotBeFraud_OtherCountry() {
+        Trader trader = new Trader("Zorro", "Berlin", "Italy");
+        Transaction transaction = new Transaction(trader, 1001);
+        assertFalse(fraudRule.isFraud(transaction));
+    }
+
+}
