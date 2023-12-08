1d7ff127ea489690d4388106d16cf6244623cd3b
Игорь Эглит <igeglits@gmail.com>
2023-12-08 22:24:48 +0200

    lesson 10 Fraud

--- src/test/java/lv/javaguru/java1/student_igor_eglit/lesson_10_project_fraud_detector/step_1/frauddetector/FraudRule2Test.java
+++ src/test/java/lv/javaguru/java1/student_igor_eglit/lesson_10_project_fraud_detector/step_1/frauddetector/FraudRule2Test.java
@@ -1,9 +1,5 @@
 package lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.step_1.frauddetector;
 
-import lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.frauddetector.FraudRule;
-import lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.frauddetector.FraudRule2;
-import lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.frauddetector.Trader;
-import lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.frauddetector.Transaction;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
