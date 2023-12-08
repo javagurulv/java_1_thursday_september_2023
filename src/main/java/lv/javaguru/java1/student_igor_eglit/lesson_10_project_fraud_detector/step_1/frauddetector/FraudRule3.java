871ab55518fcf67d13e6e18e99cb5c2c04e10f69
Игорь Эглит <igeglits@gmail.com>
2023-12-08 23:00:56 +0200

    lesson 10 Fraud

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_10_project_fraud_detector/step_1/frauddetector/FraudRule3.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_10_project_fraud_detector/step_1/frauddetector/FraudRule3.java
@@ -1,7 +1,9 @@
 package lv.javaguru.java1.student_igor_eglit.lesson_10_project_fraud_detector.step_1.frauddetector;
 
+import org.jetbrains.annotations.NotNull;
+
 class FraudRule3 implements FraudRule {
-    public boolean isFraud(Transaction transaction) {
+    public boolean isFraud(@NotNull Transaction transaction) {
         Trader trader = transaction.getTrader();
         String city = trader.getCity();
         return city.equals("Sydney");
