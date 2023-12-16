8d44c043ca6eb354a9f8742a486193f520c4a605
Igor Eglit <igeglits@gmail.com>
2023-12-16 18:12:49 +0200

    lesson 11 10

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_11_project_geometry_shape/ShapeUtil.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_11_project_geometry_shape/ShapeUtil.java
@@ -7,12 +7,23 @@ class ShapeUtil {
     public Circle createRandomCircle() {
         return new Circle(new Random().nextInt(10));
     }
+
     public Square createRandomSquare() {
         return new Square(new Random().nextInt(10));
     }
-    public Rectangle createRandomRectangle(){
-        return new Rectangle(new Random().nextInt(10),new Random().nextInt(10));
+
+    public Rectangle createRandomRectangle() {
+        return new Rectangle(new Random().nextInt(10), new Random().nextInt(10));
     }
 
+    public Shape createRandomShape() {
+        var shapeChoose = new Random().nextInt(3);
+        return switch (shapeChoose) {
+            case 0 -> createRandomCircle();
+            case 1 -> createRandomSquare();
+            case 2 -> createRandomRectangle();
+            default -> null;
+        };
+    }
 }
 
