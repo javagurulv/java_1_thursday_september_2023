5ca2076e568a3dfa390265f23c48a0ad35d76709
viktors.aleksejevs <viktors6@inbox.lv>
2023-12-18 13:54:04 +0200

    Circle

--- src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/lessoncode/Lesson11Shape/Circle.java
+++ src/main/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/lessoncode/Lesson11Shape/Circle.java
@@ -1,6 +1,24 @@
 package lv.javaguru.java1.student_viktors_aleksejevs.lesson_2_variables.lessoncode.Lesson11Shape;
 
-//class Circle extends Shape {
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 2 * Math.PI * radius;
+    }
+    @Override
+    public double calculatePerimeter(){
+        return Math.PI * (radius * radius);
+    }
+}
 
 
-//}
+
+
