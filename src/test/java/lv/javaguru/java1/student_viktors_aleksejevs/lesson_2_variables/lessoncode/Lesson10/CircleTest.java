7dc603b8dcd39d911fd78f9992dd4d8b22ccae94
viktors.aleksejevs <viktors6@inbox.lv>
2023-12-18 13:29:30 +0200

    CircleTest

---
+++ src/test/java/lv/javaguru/java1/student_viktors_aleksejevs/lesson_2_variables/lessoncode/Lesson10/CircleTest.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_viktors_aleksejevs.lesson_2_variables.lessoncode.Lesson10;
+
+import lv.javaguru.java1.student_viktors_aleksejevs.lesson_2_variables.lessoncode.Lesson11Shape.Circle;
+import org.junit.jupiter.api.Test;
+
+class CircleTest {
+
+    @Test
+    public void calculateArea() {
+        Circle circle = new Circle("X",2);
+
+    }
+
+}
