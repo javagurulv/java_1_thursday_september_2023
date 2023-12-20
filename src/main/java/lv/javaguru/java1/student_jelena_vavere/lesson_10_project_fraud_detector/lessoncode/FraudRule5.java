72cac08cac8a9cfebed08aea76f652314f33aa4f
jelenavavere <jelenavavere@inbox.lv>
2023-12-20 17:49:11 +0200

    FraudRule5

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_10_project_fraud_detector/lessoncode/FraudRule5.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_10_project_fraud_detector.lessoncode;
+class FraudRule5 implements FraudRule{
+    public boolean isFraud (Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getCountry().equals("Germany")
+                && transaction.getAmount() > 1000;
+    }
+    public String getRuleName () {return "FraudRule5";}
+}
