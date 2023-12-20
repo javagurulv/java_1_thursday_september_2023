40a22a360c54b1ef396847180f2ec4d8ea49597f
jelenavavere <jelenavavere@inbox.lv>
2023-12-20 17:44:27 +0200

    FraudRule3

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_10_project_fraud_detector/lessoncode/FraudRule3.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_10_project_fraud_detector.lessoncode;
+
+class FraudRule3 implements FraudRule {
+
+    public boolean isFraud(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getCity().equals("Sydney");
+    }
+    public String getRuleName() {return  "FraudRule3"; }
+}
