201b539612ef5b0cf29bfed9e7b8c57f8bf684d0
jelenavavere <jelenavavere@inbox.lv>
2023-12-20 19:03:07 +0200

    Shape

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_11_project_geometry_shape/geometry/shapes/Shape.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_11_project_geometry_shape.geometry.shapes;
+
+abstract class Shape {
+    String nameOfFigure;
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+
+}
