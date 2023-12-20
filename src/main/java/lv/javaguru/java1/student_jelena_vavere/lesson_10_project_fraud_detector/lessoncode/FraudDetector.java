636f7a438ebe9b3793b7a5689969c60b4b6e3136
jelenavavere <jelenavavere@inbox.lv>
2023-12-20 17:40:29 +0200

    FraudDetectionResult

--- src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_10_project_fraud_detector/lessoncode/FraudDetector.java
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_10_project_fraud_detector/lessoncode/FraudDetector.java
@@ -1,8 +1,33 @@
 package lv.javaguru.java1.student_jelena_vavere.lesson_10_project_fraud_detector.lessoncode;
 
-class FraudDetector {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+public class FraudDetector {
+    private List<FraudRule> fraudRules;
 
-    boolean isFraud(Transaction transaction) {
+    public FraudDetector() {
+        fraudRules = new ArrayList<>();
+        fraudRules.add(new FraudRule1());
+        fraudRules.add(new FraudRule2());
+        fraudRules.add(new FraudRule3());
+        fraudRules.add(new FraudRule4());
+    }
+
+    public FraudDetectionResult isFraud(Transaction transaction) {
+        for (FraudRule fraudRule : fraudRules) {
+            if (fraudRule.isFraud(transaction)) {
+                return new FraudDetectionResult(true, fraudRule.getRuleName());
+            }
+        }
+        return new FraudDetectionResult(false, null);
+
+    }
+}
+
+
+
+   /* boolean isFraud(Transaction transaction) {
         Trader trader = transaction.getTrader();
         String traderName = trader.getFullName();
         String city = trader.getCity();
@@ -11,18 +36,25 @@ class FraudDetector {
         if (traderName.equals("Pokemon")) {
             return true;
         }
-
             if (amount> 1000000){
                 return  true;
             }
             if (trader.getCity().equals("Sidney")){
                 return true;
             }
-            if (trader.getCountry().equals("Jamica")) {
+            if (trader.getCountry().equals("Jamaica")) {
+                return true;
+            }
+
+            if (trader.getCountry().equals("Germany")) {
+                return true;
+            }
+
+            if (amount> 1000) {
                 return true;
             }
             return  false;
         }
     }
-
+*/
 
