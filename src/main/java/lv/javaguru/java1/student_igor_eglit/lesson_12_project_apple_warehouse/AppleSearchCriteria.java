5b1625d78a7d6907c9a589fa427417931368ac99
Igor Eglit <igeglits@gmail.com>
2023-12-27 19:25:16 +0200

    lesson 12 task 12

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_12_project_apple_warehouse/AppleSearchCriteria.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_12_project_apple_warehouse/AppleSearchCriteria.java
@@ -1,5 +1,6 @@
 package lv.javaguru.java1.student_igor_eglit.lesson_12_project_apple_warehouse;
 
-public interface AppleSearchCriteria {
+@FunctionalInterface
+interface AppleSearchCriteria {
     boolean test(Apple apple);
 }
