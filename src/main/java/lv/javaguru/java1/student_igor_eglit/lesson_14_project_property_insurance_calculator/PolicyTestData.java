4c5a68cfcb406103c956206c59240da073c23333
Igor Eglit <igeglits@gmail.com>
2024-01-12 21:03:40 +0200

    lesson 14 risk type and calc added

--- src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_14_project_property_insurance_calculator/PolicyTestData.java
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_14_project_property_insurance_calculator/PolicyTestData.java
@@ -10,13 +10,15 @@ class PolicyTestData {
         var insuredObject_1_1 = new InsuredObject("House 1", subObject_1_1, subObject_1_2);
         return new Policy(new StringBuilder("1-C-1346-LV"), Status.REGISTERED, insuredObject_1_1);
     }
+
     //Second Policy
-static Policy example2() {
-    var subObject_2_1 = new SubObject("TV", new BigDecimal("500.00"), RiskType.FIRE);
-    var subObject_2_2 = new SubObject("PC", new BigDecimal("102.51"), RiskType.THEFT);
-    var insuredObject_2_1 = new InsuredObject("House 1", subObject_2_1, subObject_2_2);
-    return  new Policy(new StringBuilder("1-C-1346-LV"), Status.REGISTERED, insuredObject_2_1);
-}
+    static Policy example2() {
+        var subObject_2_1 = new SubObject("TV", new BigDecimal("500.00"), RiskType.FIRE);
+        var subObject_2_2 = new SubObject("PC", new BigDecimal("102.51"), RiskType.THEFT);
+        var insuredObject_2_1 = new InsuredObject("House 1", subObject_2_1, subObject_2_2);
+        return new Policy(new StringBuilder("1-C-1346-LV"), Status.REGISTERED, insuredObject_2_1);
+    }
+
     public static void main(String[] args) {
 
         System.out.println(example1());
---
+++ src/main/java/lv/javaguru/java1/student_igor_eglit/lesson_14_project_property_insurance_calculator/Premium_FLOOD.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_igor_eglit.lesson_14_project_property_insurance_calculator;
+
+import java.math.BigDecimal;
+
+class Premium_FLOOD {
+    public BigDecimal calculate(Policy policy) {
+
+        final BigDecimal threshold = new BigDecimal("15");
+        final BigDecimal defaultCoef = new BigDecimal("0.11");
+        final BigDecimal highCoef = new BigDecimal("0.05");
+
+        var insuredSumCalculation = new InsuredSumCalculation();
+        var sum = insuredSumCalculation.toSumSubObjectsInsuredPrice(policy, RiskType.FLOOD);
+        var sumToImplementCoef = new InsuredSumMultiplyCoef();
+
+        return sumToImplementCoef.toApplieCoef(sum, threshold, defaultCoef, highCoef);
+
+    }
+}
