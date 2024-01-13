d7d17a128e0ad98cef83fbbd665117fc5c440165
jelenavavere <jelenavavere@inbox.lv>
2024-01-13 17:24:21 +0200

    Apple

---
+++ src/main/java/lv/javaguru/java1/student_jelena_vavere/lesson_12_project_apple_warehouse/Apple.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_jelena_vavere.lesson_12_project_apple_warehouse;
+
+class Apple {
+    private String color;
+    private int weight;
+
+    public Apple (String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+    public String getColor(){return color;}
+    public int getWeight(){return weight;}
+
+}
