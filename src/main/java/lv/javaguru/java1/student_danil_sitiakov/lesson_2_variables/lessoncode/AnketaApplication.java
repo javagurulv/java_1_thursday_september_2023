f4feec32c10ad7ee64634919fdeec40c945a83f2
igor eglit <igeglits@gmail.com>
2024-02-04 14:58:54 +0200

    fix fail

--- src/main/java/lv/javaguru/java1/student_danil_sitiakov/lesson_2_variables/lessoncode/AnketaApplication.java
+++ src/main/java/lv/javaguru/java1/student_danil_sitiakov/lesson_2_variables/lessoncode/AnketaApplication.java
@@ -5,12 +5,12 @@ class AnketaApplication {
    public static void main(String[] args) {
 
        String person;
-       Anketa question1 = new Anketa("question1?", person:"Petja" );
-       Anketa question2 = new Anketa("question2?", person:"Vasja");
+       Anketa question1 = new Anketa("question1?", "Petja" );
+       Anketa question2 = new Anketa("question2?", "Vasja");
 
-       Anketa questionnaire = new Anketa(person:"Vasja, question1, question2");
+       Anketa questionnaire = new Anketa("Vasja, question1, question2","Vasja");
 
-       person; = Anketa.getPerson();
+       person = Anketa.getPerson();
        Anketa q1 = questionnaire.getQuestion1();
        Anketa q2 = questionnaire.getQuestion2();
 
